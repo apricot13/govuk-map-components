b527e38ee0fa1eced92a744501ca4e2e70a55a3f
Han <apricot13@users.noreply.github.com>
2026-01-22 21:48:54 +0000

    pdf component

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import "./style.scss";
 export { MyBox } from "./components/example-component/example-component";
 export { LoadingSpinner } from "./components/loading-spinner/loading-spinner";
 export { StatusChecker } from "./components/status-checker/status-checker";
+export { PdfComponent } from "./components/pdf/pdf";
