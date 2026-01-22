af986c6b4b2da2c24d6bc052c83c98b62f94d8b6
Han <apricot13@users.noreply.github.com>
2026-01-22 21:23:50 +0000

    add new components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import "./style.scss";
 export { MyBox } from "./components/example-component/example-component";
+export { LoadingSpinner } from "./components/loading-spinner/loading-spinner";
 export { StatusChecker } from "./components/status-checker/status-checker";
